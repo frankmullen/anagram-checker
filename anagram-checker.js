fe14f71b019e68acce47f2f70412078fcecfc6ea
Frank Mullen <frankmullen@gmail.com>
2016-12-10 00:57:21 +0000

    working code

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,49 +1,70 @@
-var word1 = listen;
-var word2;
-var oldWordArray=[];
-var newWordArray=[];
 
-console.log("word is: " + word1);
-convertWordToArray(word1);
-convertArrayToAlphabeticalArray(oldWordArray);
-console.log("new word is: " + newWordArray.toString());
+
+
+function startCheck() {
+	document.getElementById("result").innerHTML=("");
+	var word1 = document.getElementById("word1").value;
+	console.log("word is: " + word1);
+	var word2 = document.getElementById("word2").value;
+	console.log("word is: " + word2);
+	var newWord1 = convertWordToAlphabeticalArray(word1).join("");
+	var newWord2 = convertWordToAlphabeticalArray(word2).join("");
+	console.log(newWord1);
+	console.log(newWord2);
+	checkAnagram(newWord1, newWord2);
+
+};
+
 
 function checkWords(word1, word2) {
 	array1;
 	array2;
-}
+};
 
-function convertWordToArray(word) {
-
+function convertWordToAlphabeticalArray(word) {
+	
 	var wordArray = [];
 	for (var i=0; i<word.length; i++) {
-		wordArray[i] = word.charAt[0];
+		if (word.charAt(i).match(/[A-Za-z]/)){
+			wordArray[i] = word.charAt(i).toLowerCase();
+		}
 	}
-	oldWordArray = wordArray;
-}
 
-function convertArrayToAlphabeticalArray(wordArray) {
+	console.log(wordArray.toString());
+	return convertAlphabetical(wordArray);
 	
-	var newArray;
+};
 
-	for (var i=0; i<wordArray.length; i++) {
-		if (i === 0) {
-			newArray[0] = wordArray[0];
-		} else {
-			for (j=0; j<newArray.length; j++) {
-				//see where the new letter slots in
-				if (wordArray[i]<newArray[j]) {
-					//make new last character in array, same as previous last
-					newArray.push(newArray[newArraylength-1]);
-					//move every character after the insertion point to a position one higher
-					for (var k=newArray.length-2; k>j; k--) {
-						newArray[k] = newArray[k-1];
-					}
-					//insert character from wordArray
-					newArray[j] = wordArray[i];
-			} else {
-				newArray.push(wordArray[i]);
+function convertAlphabetical(wordArray) {
+
+	for (var i=1; i<wordArray.length; i++) {
+
+		for (var j=0; j<=i; j++){
+			
+			if (wordArray[i] < wordArray[j]) {
+				var moveLetter = wordArray[i];
+				//move every character after the insertion point j, but no greater than i to a position one higher
+				for (var k=i; k>j; k--) {
+					wordArray[k] = wordArray[k-1];
+				}
+				//insert character from wordArray
+				wordArray[j] = moveLetter;
 			}
 		}
 	}
+	console.log(wordArray.toString());
+	return wordArray
+};
+
+function checkAnagram(word1, word2) {
+	if (word1 === word2) {
+		document.getElementById("result").innerHTML=("Yes, the words are anagrams!");
+	} else {
+		document.getElementById("result").innerHTML=("No, the words are not anagrams!");
+	}
 }
+
+function testFunction(){
+	return "hello";
+}
+
